04109616e97c7cb858e2cf57d7d943baadb0fde6
marvin.deckmyn <marvin.deckmyn@student.howest.be>
2020-03-24 23:23:23 +0100

    sjongejonge

--- public/assets/js/register.js
+++ public/assets/js/register.js
@@ -3,6 +3,8 @@
 const USERNAME = "input[name='gebruikersnaam']";
 const REGISTER = "input[type='submit']";
 const FOUTMELDING = "label[for='foutmeldingusername']";
+const WACHTWOORD = "input[name='wachtwoord']";
+const WACHTWOORDBEVESTIGING = "input[name='wachtwoordbevestigen']";
 document.addEventListener("DOMContentLoaded", init);
 
 function init() {
@@ -13,16 +15,28 @@ function validateUsername(username) {
     return /^[0-9a-zA-Z]{1,15}$/.test(username);
 }
 
+function validatePassword(password, confirmedpassword) {
+    return password === confirmedpassword;
+}
+
 function validateForm(e) {
 
     let username = document.querySelector(USERNAME).value;
+    let password = document.querySelector(WACHTWOORD).value;
+    let confirmedpassword = document.querySelector(WACHTWOORDBEVESTIGING).value;
 
     if (validateUsername(username) === false) {
         document.querySelector(FOUTMELDING).innerHTML = "Je gebruikersnaam mag alleen bestaan uit letters en/of cijfers & je gebruikersnaam mag maximum 15 karakters bevatten.";
         return false;
     }
 
+    else if (validatePassword(password, confirmedpassword) === false) {
+        document.querySelector(FOUTMELDING).innerHTML = "Je wachtwoord komt niet overeen.";
+        return false;
+    }
+
     else {
+        document.querySelector(FOUTMELDING).innerHTML = "";
         return true;
     }
 }
